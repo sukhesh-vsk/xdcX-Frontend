58b16032c335ec1ca1278153e93b3067de932171
sukhesh-vsk <sukhesh.vsk2005@gmail.com>
2024-01-13 19:44:17 +0530

    Added Styles to P2P Component

--- src/components/P2P/PtP.jsx
+++ src/components/P2P/PtP.jsx
@@ -1,17 +1,49 @@
 import React from 'react'
 import { Grid, TextField, Button } from '@mui/material'
+import { styled } from '@mui/system';
 
 import './PtP.css'
 import payment from '../../assets/img/payment.png'
 import xdc from '../../assets/img/xdc.png'
 
 const PtP = () => {
+  const CustomTextField = styled(TextField)({
+    '& .MuiFormLabel-root, .MuiInputBase-input': {
+      color: '#ffffffcc',
+      backgroundColor: '#2C71FC',
+      fontFamily: "Circular Pro Medium,sans-serif",
+    },
+    '& .MuiInputBase-root': {
+      backgroundColor: '#2C71FC',
+      borderRadius: '15px',
+    },
+    '& .MuiInputLabel-root.MuiInputLabel-shrink': {
+      color: '#000000cc',
+      fontSize: '1rem',
+      textStroke: '0.5px #000000cc',
+      WebkitTextStroke: '0.5px #000000cc',
+      background: 'none',
+    },
+    '& .MuiInputBase-input::placeholder': {
+      color: '#ffffffcc',
+      fontFamily: "Circular Pro Medium,sans-serif",
+    },
+    'MuiInputBase-input': {
+      color: 'red',
+    }
+  });
+
+  const CustomButton = styled(Button) ({
+    background: "linear-gradient(to bottom right, #4294E3, #8F12FD)",
+    borderRadius: '10px',
+  });
+  
   return (
     <Grid container>
-      <Grid item xs={12}>
+      <Grid item xs={12} className='mt-3'>
         <span className='title'>P2P Fund Transfer</span>
       </Grid>
-      <Grid item xs={8} className="px-5">
+      <Grid item xs={7} style={{paddingLeft: "11rem"}} className="my-auto">
         <Grid item xs={12} className="my-3">
           <span className='colordim ps-2'>Your balance</span>
           <Grid item className="d-flex showbalance" xs={12}>
@@ -20,21 +52,21 @@ const PtP = () => {
           </Grid>
         </Grid>
         <Grid item xs={12} className="d-flex flex-column my-4">
-          <TextField
+          <CustomTextField
             id="outlined-textarea"
             label="Recipient Address"
             placeholder="Enter Recipient Address"
             multiline
-            className='mb-3 w-50'
+            className={`mb-3 w-75`}
           />
-          <TextField
+          <CustomTextField
             id="outlined-textarea"
             label="Amount"
             placeholder="Amount to transfer (XDC)"
             multiline
-            className='mb-3 w-50'
+            className='mb-3 w-75'
           />
-          <Button variant="contained" className='my-4 w-25'>Transfer</Button>
+          <CustomButton variant="contained" className='my-4 transfer-btn' style={{marginLeft: "26%"}}>Transfer</CustomButton>
         </Grid>
       </Grid>
       <Grid item xs={4}>
