e16cd3470682ff35b27d93f514802545b56e26f4
sukhesh-vsk <sukhesh.vsk2005@gmail.com>
2024-01-13 20:06:45 +0530

    Added Banner Subtext

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,8 +3,9 @@ import './Banner.css'
 
 const Banner = () => {
   return (
-    <div className='banner-container d-flex'>
+    <div className='banner-container d-flex flex-column justify-content-center ms-5 ps-5 pb-5'>
         <span>Stake Smart, <br/>Pay Smarter <br/>with xdcX! </span>
+        <p class="color-dim">Stake XDC and use xdcX while earning staking rewards.</p>
     </div>
   )
 }
