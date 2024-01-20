64cad98d9ac407f0726e919da11e9ba5c2a807ce
sukhesh-vsk <sukhesh.vsk2005@gmail.com>
2024-01-20 12:47:46 +0530

    Added Navbar Component

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
-import './Banner.css'
+import React from 'react';
+
+import './Banner.css';
+import banner from '../../assets/img/blockchain.png';
 
 const Banner = () => {
   return (
-    <div className='banner-container d-flex flex-column justify-content-center ms-5 ps-5'>
+    <section className='container-fluid banner-container row d-flex align-items-center'>
+      <div className='col-md-7 banner-text'>
         <span className='shadow-gradient' />
-        <span className="head-text me-auto text-start">Stake Smart, <br/>Pay Smarter <br/>with xdcX! </span>
+        <span className="head-text text-start">Stake Smart, <br/>Pay Smarter <br/>with xdcX! </span>
         <p class="color-dim">Stake XDC and use xdcX while earning staking rewards.</p>
-    </div>
+      </div>
+      <div className='col-md-4 text-center'>
+        <img src={banner} className='banner-img w-100' alt='banner-img' />
+      </div>
+    </section>
   )
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,24 +1,58 @@
 import React from 'react'
 import CustomButton from '../Custom/CustomButton'
-
+import './Navbar.css'
 const Navbar = () => {
     return (
-        <nav class="navbar navbar-expand-lg mx-5 mt-3 sticky-top">
-            <div class="container-fluid d-flex space-around">
-                <span>
-                    <a class="navbar-brand head-text" href="#">XDCX</a>
-                </span>
-                <div id="navbarSupportedContent">
-                    <CustomButton 
-                        className="btn btn-green"
-                        type="submit"
-                        style={{width: "180px"}}
-                    >
-                        Connect to Wallet
-                    </CustomButton>
-                </div>
+        <nav className="navbar navbar-expand-lg px-5 pt-5">
+            <a className="navbar-brand head-text m-0" href="#">XDCX</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse d-flex justify-content-center stroke" id="navbarSupportedContent">
+                <ul className="navbar-nav mr-auto d-flex justify-content-around">
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#">HOME</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">LINK</a>
+                    </li>
+                </ul>
+            </div>
+                <CustomButton
+                    className="btn btn-green"
+                    type="submit"
+                    style={{ width: "180px" }}
+                >
+                    Connect to Wallet
+                </CustomButton>
+            <div>
+
             </div>
         </nav>
+        // <nav className="navbar navbar-expand-lg mx-5 mt-3 sticky-top">
+        //     <div className="container-fluid d-flex space-around">
+        //         <span>
+        //             <a className="navbar-brand head-text" href="#">XDCX</a>
+        //         </span>
+        //         <div>
+        //             <ul className='d-flex justify-content-around'>
+        //                 <li>HOME</li>
+        //                 <li>HOME</li>
+        //                 <li>HOME</li>
+        //             </ul>
+        //         </div>
+        //         <div id="navbarSupportedContent">
+        //             <CustomButton 
+        //                 className="btn btn-green"
+        //                 type="submit"
+        //                 style={{width: "180px"}}
+        //             >
+        //                 Connect to Wallet
+        //             </CustomButton>
+        //         </div>
+        //     </div>
+        // </nav>
     )
 }
 
